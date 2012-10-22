43b362302ef61c3b34a9756d9a1dc05e244d9c64
Genival Jr <moreno.alado@gmail.com>
2012-10-22 15:17:47 -0300

    Upado os arquivos.

---
+++ header.php
@@ -0,0 +1,36 @@
+<?php include("core.php"); ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta name="description" content="<?php echo $core_desc ?>" />
+    <meta name="keywords" content="<?php echo $core_keywords ?>" />
+    <meta name="author" content="Genival Júnior" />
+    <meta charset="UTF-8" />
+    <link rel="shortcut icon" href="images/favicon.png" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <link rel="stylesheet" type="text/css" href="stylesheets/application.css"/>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+    <script>!window.jQuery && document.write('<script src="javascripts/jquery-1.7.js"><\/script>')</script>
+      <!-- asynchronous google analytics
+      change the UA-XXXXX-X to be your site's ID -->
+      <script>
+        var _gaq = [['_setAccount', 'UA-XXXXX-X'], ['_trackPageview']];
+        (function(d, t) {
+          var g = d.createElement(t),
+          s = d.getElementsByTagName(t)[0];
+          g.async = true;
+          g.src = '//www.google-analytics.com/ga.js';
+          s.parentNode.insertBefore(g, s);
+        })(document, 'script');
+      </script>
+      <link rel="stylesheet" type="text/css" href="<?php servidor(); ?>javascripts/boot_modal/css/bootstrap.css" media="all">
+      <script type="text/javascript" src="<?php servidor(); ?>javascripts/boot_modal/js/bootstrap.js"></script>
+      <script type="text/javascript" src="<?php servidor(); ?>javascripts/jquery.linkify-1.0.js"></script>
+      <script type="text/javascript" src="<?php servidor(); ?>javascripts/jquery.jtwitter.js"></script>
+      <script type="text/javascript" src="<?php servidor(); ?>javascripts/jquery.cycle.all.js"></script>
+      <script type="text/javascript" src="<?php servidor(); ?>javascripts/java.js"></script>
+    <!--[if lt IE 7 ]> <body class="ie6"> <![endif]-->
+    <!--[if IE 7 ]>    <body class="ie7"> <![endif]-->
+    <!--[if IE 8 ]>    <body class="ie8"> <![endif]-->
+    <!--[if IE 9 ]>    <body class="ie9"> <![endif<]-->
+    <!--[if (gt IE 9)|!(IE)]><!-->  <!--<![endif]-->
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include("header.php") ?>
+    <title>Título da Página</title>
+  </head>
+  <body>
+    <div id="header">
+    </div>
+    <div id="content">
+    </div>
+    <div id="navigation">
+    </div>
+    <div id="sidebar">
+    </div>
+    <div id="footer">
+    <?php $data=date("Y"); echo $data; ?> <!-- Data Atual -->
+    </div>
+  </body>
+</html>
+
